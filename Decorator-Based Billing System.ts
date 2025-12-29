887f9d0fdf629c87336c90867ef5f97776e2dcb9
Praveena Chintha <chinthapraveena194@gmail.com>
2025-12-29 22:20:09 +0530

    Create Decorator-Based Billing System.ts

---
+++ Decorator-Based Billing System.ts
@@ -0,0 +1,82 @@
+abstract class Beverage {
+  abstract getDescription(): string;
+  abstract getCost(): number;
+}
+class Espresso extends Beverage {
+  getDescription(): string {
+    return "Espresso";
+  }
+
+  getCost(): number {
+    return 80;
+  }
+}
+
+class LemonTea extends Beverage {
+  getDescription(): string {
+    return "LemonTea";
+  }
+
+  getCost(): number {
+    return 40;
+  }
+}
+
+class Sugar extends Beverage {
+  private beverage: Beverage;
+
+  constructor(beverage: Beverage) {
+    super();
+    this.beverage = beverage;
+  }
+
+  getDescription(): string {
+    return `${this.beverage.getDescription()} + Sugar`;
+  }
+
+  getCost(): number {
+    return this.beverage.getCost() + 10;
+  }
+}
+
+class Honey extends Beverage {
+  private beverage: Beverage;
+
+  constructor(beverage: Beverage) {
+    super();
+    this.beverage = beverage;
+  }
+
+  getDescription(): string {
+    return `${this.beverage.getDescription()} + Honey`;
+  }
+
+  getCost(): number {
+    return this.beverage.getCost() + 20;
+  }
+}
+
+class WhippedCream extends Beverage {
+  private beverage: Beverage;
+
+  constructor(beverage: Beverage) {
+    super();
+    this.beverage = beverage;
+  }
+
+  getDescription(): string {
+    return `${this.beverage.getDescription()} + WhippedCream`;
+  }
+
+  getCost(): number {
+    return this.beverage.getCost() + 15;
+  }
+}
+const order1 = new Honey(new WhippedCream(new Espresso()));
+const order2 = new Sugar(new Sugar(new LemonTea()));
+
+console.log("Order 1:", order1.getDescription()); 
+console.log("Cost 1: ₹", order1.getCost());      
+
+console.log("Order 2:", order2.getDescription()); 
+console.log("Cost 2: ₹", order2.getCost());      
