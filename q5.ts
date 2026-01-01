db14e728f1d6a1a4e057009ee4a2c620d8bf919c
Praveena Chintha <chinthapraveena194@gmail.com>
2026-01-01 21:27:57 +0530

    Create q5.ts

---
+++ q5.ts
@@ -0,0 +1,43 @@
+interface Product {
+  getDescription(): string
+}
+
+class Laptop implements Product {
+  constructor(private name: string, private price: number) {}
+  getDescription(): string {
+    return `Laptop: ${this.name}, Price: $${this.price}`
+  }
+}
+
+class Mobile implements Product {
+  constructor(private name: string, private price: number) {}
+  getDescription(): string {
+    return `Mobile: ${this.name}, Price: $${this.price}`
+  }
+}
+
+class Tablet implements Product {
+  constructor(private name: string, private price: number) {}
+  getDescription(): string {
+    return `Tablet: ${this.name}, Price: $${this.price}`
+  }
+}
+
+type ProductConstructor = new (name: string, price: number) => Product
+
+const productMap: Record<string, ProductConstructor> = {
+  Laptop,
+  Mobile,
+  Tablet
+}
+
+class ProductFactory {
+  static createProduct(type: string, name: string, price: number): Product {
+    const ProductClass = productMap[type]
+    if (!ProductClass) throw new Error("Unknown product type")
+    return new ProductClass(name, price)
+  }
+}
+
+const t = ProductFactory.createProduct("Tablet", "Galaxy Tab", 1100)
+console.log(t.getDescription())
